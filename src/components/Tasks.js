e7cdcca8e06ccdaf5d05506bccf64106614d461b
AMRRAGHEB118 <amrragheb118@gmail.com>
2023-08-04 11:24:22 +0300

    handle showing tasks by state

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,17 +1,26 @@
 import { useParams } from "react-router-dom";
 import { Card, CardContent, Typography } from "@mui/material";
-
 import Task from "./Task";
 
 export default function Tasks({ tasks, handle_check }) {
   const { state } = useParams();
   let tasksLoader = [];
+  let tasks_to_be_rendered;
   if (!state || state === "all") {
-    tasksLoader = tasks.map((task) => (
-      <Task key={task.id} task={task} handle_check={handle_check} />
-    ));
-  }
+    tasks_to_be_rendered = tasks;
+  } else if (state === "done") {
+    tasks_to_be_rendered = tasks.filter((task) =>
+      task.is_complete ? task : null
+    );
+  } else
+    tasks_to_be_rendered = tasks.filter((task) =>
+      !task.is_complete ? task : null
+    );
 
+  tasksLoader = tasks_to_be_rendered.map((task) => (
+    <Task key={task.id} task={task} handle_check={handle_check} />
+  ));
+  
   return (
     <>
       <Card sx={{ background: "#c1c1c17a", margin: "10px", minWidth: 275 }}>
