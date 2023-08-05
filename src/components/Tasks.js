2420a9c06cd59ad84ef5d72f4b922c30e9e89963
AMRRAGHEB118 <amrragheb118@gmail.com>
2023-08-05 19:50:40 +0300

    handle caching for tasks by useMemo

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,29 +1,36 @@
 import { useParams } from "react-router-dom";
 import { Card, CardContent, Typography } from "@mui/material";
 import Task from "./Task";
-import { useContext } from "react";
+import { useContext, useMemo } from "react";
 import { TasksContext } from "../context/tasksContext";
 
 export default function Tasks() {
-  const {tasks} = useContext(TasksContext)
+  const { tasks } = useContext(TasksContext);
   const { state } = useParams();
+
   let tasksLoader = [];
   let tasks_to_be_rendered;
+
+  const completed_tasks = useMemo(() => {
+    return tasks.filter((task) => (task.is_complete ? task : null));
+  }, [tasks]);
+
+  const not_completed_tasks = useMemo(() => {
+    return tasks.filter((task) => (!task.is_complete ? task : null));
+  }, [tasks]);
+
   if (!state || state === "all") {
     tasks_to_be_rendered = tasks;
   } else if (state === "done") {
-    tasks_to_be_rendered = tasks.filter((task) =>
-      task.is_complete ? task : null
-    );
-  } else
-    tasks_to_be_rendered = tasks.filter((task) =>
-      !task.is_complete ? task : null
-    );
+    tasks_to_be_rendered = completed_tasks;
+  } else {
+    tasks_to_be_rendered = not_completed_tasks;
+  }
 
   tasksLoader = tasks_to_be_rendered.map((task) => (
     <Task key={task.id} task={task} />
   ));
-  
+
   return (
     <>
       <Card sx={{ background: "#c1c1c17a", margin: "10px", minWidth: 275 }}>
